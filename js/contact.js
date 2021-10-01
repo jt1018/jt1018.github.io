4769d28e53dd97361e753b8faa4211a8ed9ad9d5
jt1018 <48070252+jt1018@users.noreply.github.com>
2021-09-30 23:30:54 -0400

    t

--- js/contact.js
+++ js/contact.js
@@ -1,19 +1,19 @@
-    (function() {
+  //  (function() {
         // https://dashboard.emailjs.com/admin/integration
-        emailjs.init('jdtaylor');
-    })();
+  //      emailjs.init('jdtaylor');
+  //  })();
 
-    window.onload = function() {
-        document.getElementById('contact-form').addEventListener('submit', function(event) {
-            event.preventDefault();
+  //  window.onload = function() {
+  //      document.getElementById('contact-form').addEventListener('submit', function(event) {
+  //          event.preventDefault();
             // generate a five digit number for the contact_number variable
-            this.contact_number.value = contact_number;
+  //          this.contact_number.value = contact_number;
             // these IDs from the previous steps
-            emailjs.sendForm('contact_service', 'contact_form', this)
-                .then(function() {
-                    console.log('SUCCESS!');
-                }, function(error) {
-                    console.log('FAILED...', error);
-                });
-        });
-    }
+  //          emailjs.sendForm('contact_service', 'contact_form', this)
+    //            .then(function() {
+    //                console.log('SUCCESS!');
+    //            }, function(error) {
+    //                console.log('FAILED...', error);
+    //            });
+  //      });
+  //  }
